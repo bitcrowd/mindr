2bf3a8ba06cf9ab1a3efeb4cd4a42122d0df0b93
Hannah Voget <hannah@bitcrowd.net>
2025-12-10 17:25:16 +0100

    Add menu item

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,6 @@
+#[cfg(feature = "desktop")]
+use dioxus::desktop::{muda::*, use_muda_event_handler};
+
 use dioxus::prelude::*;
 
 use components::Mindmap;
@@ -9,11 +12,42 @@ const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/css/main.css");
 
 fn main() {
-    dioxus::launch(App);
+    #[cfg(feature = "desktop")]
+    {
+        let menu = Menu::new();
+        let edit_menu = Submenu::new("Edit", true);
+
+        edit_menu
+            .append_items(&[&MenuItem::with_id("save", "Save", true, None)])
+            .unwrap();
+
+        menu.append_items(&[&edit_menu]).unwrap();
+
+        // Create a desktop config that overrides the default menu with the custom menu
+        let config = dioxus::desktop::Config::new().with_menu(menu);
+
+        // Launch the app with the custom menu
+        dioxus::LaunchBuilder::new().with_cfg(config).launch(App);
+        return;
+    }
+
+    #[cfg(not(feature = "desktop"))]
+    {
+        dioxus::launch(App);
+    }
 }
 
 #[component]
 fn App() -> Element {
+    #[cfg(feature = "desktop")]
+    {
+        use_muda_event_handler(move |muda_event| {
+            if muda_event.id() == "save" {
+                save_to_file();
+            }
+        });
+    }
+
     rsx! {
         document::Style {
             {
@@ -27,4 +61,9 @@ fn App() -> Element {
         document::Link { rel: "stylesheet", href: MAIN_CSS }
         Mindmap {}
     }
+}
+
+#[cfg(feature = "desktop")]
+fn save_to_file() {
+    println!("{}", "Still to be implemented");
 }
