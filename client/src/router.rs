4df736bfb537376d5e2988a04a66169cb2b3e5b5
Agathe Lenclen <agathe@bitcrowd.net>
2025-12-10 17:11:33 +0100

    Add router to web client

---
+++ client/src/router.rs
@@ -0,0 +1,47 @@
+use components::Mindmap;
+use dioxus::prelude::*;
+
+use crate::components;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/css/main.css");
+
+#[derive(Clone, Debug, PartialEq, Routable)]
+pub enum Route {
+    #[route("/")]
+    Home,
+
+    #[route("/channels/:channel_id")]
+    Channel { channel_id: String },
+
+    #[route("/:any")]
+    NotFound { any: String },
+}
+
+#[component]
+fn Home() -> Element {
+    rsx! {
+       document::Style {
+           {
+               format!(
+                   " @font-face {{ font-family: 'Roboto Light'; src: url({}) format('truetype');}} ",
+                   asset!("/assets/fonts/Roboto-Light.ttf"),
+               )
+           }
+       }
+       document::Link { rel: "icon", href: FAVICON }
+       document::Link { rel: "stylesheet", href: MAIN_CSS }
+       Mindmap {},
+    }
+}
+
+#[component]
+fn Channel(channel_id: String) -> Element {
+    rsx! { "Channel page for channel with id: {channel_id}" }
+}
+
+#[component]
+fn NotFound(any: String) -> Element {
+    // TODO: Return 404 status code
+    rsx! { "Not found :(" }
+}
